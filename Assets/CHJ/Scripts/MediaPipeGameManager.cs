acf7734cba41ef5c21923a7cd80edbb874edfdbc
조현진 <111720377+joe-hy-j@users.noreply.github.com>
2024-09-11 19:50:43 +0900

    0911 미디어파이프 게임 완성

---
+++ Assets/CHJ/Scripts/MediaPipeGameManager.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MediaPipeGameManager : MonoBehaviour
+{
+    GestureData gesture;
+    GestureData answerGesture;
+
+    public GameObject buttonFactory;
+    public GameObject textFactory;
+
+    bool isGameStart =false;
+
+    Button startButton;
+    Text infoText;
+
+    private void Start()
+    {
+        InitializeGame();
+    }
+
+    private void Update()
+    {
+        if (!isGameStart) return;
+        gesture = SocketManager.instance.gesture;
+
+        if (CheckAnswer())
+        {
+            EndGame();
+        }
+    }
+    void InitializeGame()
+    {
+        // UI를 세팅한다.
+        SetUIInterface();
+    }
+
+    void StartGame()
+    {
+        isGameStart = true;
+        startButton.interactable = false;
+        // 퀴즈 정답을 세팅한다.
+        SetQuiz();
+        // UI에 퀴즈를 표시한다.
+        infoText.text = answerGesture.ToString() + "를 취하세요!" ;
+        // 파이썬 코드를 실행한다.
+        try
+        {
+            Process psi = new Process();
+            psi.StartInfo.FileName = "C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python312\\python.exe";
+            // 시작할 어플리케이션 또는 문서
+            psi.StartInfo.Arguments = "C:\\Users\\Admin\\Downloads\\temp_1726030948758.623395642\\main.py";
+            // 애플 시작시 사용할 인수
+            psi.StartInfo.CreateNoWindow = true;
+            // 새창 안띄울지
+            psi.StartInfo.UseShellExecute = false;
+            // 프로세스를 시작할때 운영체제 셸을 사용할지
+            psi.Start();
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError("Unable to launch app: " + e.Message);
+        }
+    }
+
+    bool CheckAnswer()
+    {
+        // 만약 들어온 답이 정답과 같다면
+        if (answerGesture.palm_sustained == true && gesture.palm_sustained == true)
+            return true;
+        else if (answerGesture.v_sustained == true && gesture.v_sustained == true)
+            return true;
+        else
+            return false;
+        // 아니라면
+    }
+
+    void EndGame()
+    {
+        isGameStart = false;
+
+        StartCoroutine(EndProcess());
+    }
+
+    IEnumerator EndProcess()
+    {
+        infoText.text = "성공했습니다!";  
+
+        yield return new WaitForSeconds(1);
+
+        // UI를 없앤다
+        Destroy(startButton.gameObject);
+        Destroy(infoText.gameObject);
+
+    }
+    public void SetGestureData(GestureData gesture)
+    {
+        this.gesture = gesture;
+
+        if (CheckAnswer())
+        {
+            EndGame();
+        }
+    }
+
+    void SetQuiz()
+    {
+        // 랜덤하게 손바닥 보이기와 v자 포즈 중에서 한 개를 고른다.
+        int randNum = UnityEngine.Random.Range(0, 2);
+        answerGesture = new GestureData();
+        switch (randNum)
+        {
+            // v자 보이기
+            case 0:
+                answerGesture.v_sustained = true;
+                break;
+            // 손바닥 보이기
+            case 1:
+                answerGesture.palm_sustained = true;
+                break;
+        }
+    }
+
+    void SetUIInterface()
+    {
+        // 행동 표시하는 text 1개
+        infoText = Instantiate(textFactory, GameObject.Find("GameCanvas").transform).GetComponent<Text>();
+        infoText.text = "";
+        // start Button 1개
+        startButton = Instantiate(buttonFactory, GameObject.Find("GameCanvas").transform).GetComponent<Button>();
+        // button에 onClick 되면 StartGame이 실행되어야 한다.
+        startButton.onClick.AddListener(StartGame);
+    }
+
+}
---
+++ Assets/CHJ/Scripts/SocketManager.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class GestureData
+{
+    public bool v_gesture;
+    public bool palm_visible;
+    public bool v_sustained;
+    public bool palm_sustained;
+
+    public override string ToString()
+    {
+        if (v_sustained)
+            return "V자 포즈";
+        else if (palm_sustained)
+            return "손바닥 포즈";
+        else
+            return "";
+    }
+}
+
+public class SocketManager : MonoBehaviour
+{
+    public int port = 5005; // Port to listen on
+    private UdpClient udpClient;
+    IPEndPoint remoteEndPoint;
+    private bool isListening = true;
+    public static SocketManager instance;
+
+    public GestureData gesture;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        StartListening();
+    }
+
+    void OnApplicationQuit()
+    {
+        StopListening();
+    }
+
+    private void StartListening()
+    {
+        try
+        {
+            udpClient = new UdpClient(port);
+            udpClient.BeginReceive(ReceiveCallback, null);
+            remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+            Debug.Log($"Listening for UDP packets on port {port}.");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Exception: {ex.Message}");
+        }
+    }
+
+    private void StopListening()
+    {
+        isListening = false;
+        udpClient?.Close();
+    }
+
+    private void ReceiveCallback(IAsyncResult result)
+    {
+        if (!isListening)
+            return;
+
+        try
+        {
+            byte[] receivedData = udpClient.EndReceive(result, ref remoteEndPoint);
+            string message = Encoding.UTF8.GetString(receivedData);
+            gesture = JsonUtility.FromJson<GestureData>(message);
+            Debug.Log($"Received message: {message} from {remoteEndPoint}");
+
+            // Continue listening for more data
+            udpClient.BeginReceive(ReceiveCallback, null);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"ReceiveCallback Exception: {ex.Message}");
+        }
+    }
+}
