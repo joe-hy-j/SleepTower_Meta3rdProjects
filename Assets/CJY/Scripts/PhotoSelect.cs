b443e9c9d224810556330caecc9f74ae1809e56c
Popcorn0212 <cwd8564@gmail.com>
2024-09-16 17:45:50 +0900

    0916

--- Assets/CJY/Scripts/PhotoSelect.cs
+++ Assets/CJY/Scripts/PhotoSelect.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotoSelect : MonoBehaviour
 {
     public GameObject photoSelecter;
-    bool isOn;
+    public GameObject imageSlot;
+    public Sprite image1;
+    public Sprite image2;
+    public Sprite image3;
+    public Sprite image4;
+    Image image;
+
 
     void Start()
     {
-        
+        image = imageSlot.GetComponent<Image>();
     }
 
     void Update()
@@ -17,18 +24,33 @@ public class PhotoSelect : MonoBehaviour
         
     }
 
+    public void ImageUpload1()
+    {
+        image.sprite = image1;
+    }
+
+    public void ImageUpload2()
+    {
+        image.sprite = image2;
+    }
+
+    public void ImageUpload3()
+    {
+        image.sprite = image3;
+    }
+
+    public void ImageUpload4()
+    {
+        image.sprite = image4;
+    }
+
     public void OnSelecter()
     {
-        isOn = true;
         photoSelecter.SetActive(true);
     }
 
     public void CloseSelecter()
     {
-        if (isOn)
-        {
-            photoSelecter.SetActive(false);
-            isOn = false;
-        }
+        photoSelecter.SetActive(false);
     }
 }
