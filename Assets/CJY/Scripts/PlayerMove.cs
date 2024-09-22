609f45b98f26d0f5e426be51b755c101dd6746e4
ORR-studio <jon589@naver.com>
2024-09-22 15:57:11 +0900

    시연영상 마지막 빌드

--- Assets/CJY/Scripts/PlayerMove.cs
+++ Assets/CJY/Scripts/PlayerMove.cs
@@ -1,7 +1,7 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviourPun
